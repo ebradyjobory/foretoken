e63f2bf6f22a3dfc66bc7579d5abf7b64f90e225
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-28 18:32:14 -0500

    Update alert messages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( highcharts.js )
 Rails.application.config.assets.precompile += %w( reset.css )
 Rails.application.config.assets.precompile += %w( charts.js )
+Rails.application.config.assets.precompile += %w( sweet-alert.css )
+Rails.application.config.assets.precompile += %w( sweet-alert.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
