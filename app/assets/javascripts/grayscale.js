7d166ef08f403fd6dbecada0eee75819999eba6c
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-27 23:33:20 -0500

    Update design

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -31,7 +31,7 @@ $('.navbar-collapse ul li a').click(function() {
 
 // Google Maps Scripts
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+//google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // Basic options for a simple Google Map
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        //center: new google.maps.LatLng(40.6700, -73.9400), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
